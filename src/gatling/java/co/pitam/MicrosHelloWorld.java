df0ca869f048fbb5c4ccfd9ff8b3c06cab4a84f1
birbirs <birbirsbirbirs@gmail.com>
2024-11-23 10:13:12 -0600

    updated for ptm-spring-events

--- src/gatling/java/co/pitam/MicrosHelloWorld.java
+++ src/gatling/java/co/pitam/MicrosHelloWorld.java
@@ -6,33 +6,25 @@ import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.rampUsers;
-import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class MicrosHelloWorld extends Simulation {
-    HttpProtocolBuilder req= http.baseUrl("http://192.168.49.2:30080");
+    HttpProtocolBuilder req= http.baseUrl("http://localhost:8080/");
 
     ScenarioBuilder myscenario= CoreDsl.scenario("micros")
-            .exec(http("helloworld/hero")
-                    .get("/helloworld/hero")
+            .exec(http("")
+                    .get("")
                     .check(
                             status().is(200),
                             status().not(500)
                     )
-            ).pause(3)
-            .exec(http("helloworld")
-                    .get("/helloworld")
-                    .check(
-                            status().is(200),
-                            status().not(500)
-                    )
-            ).pause(3);
+            );
 
     {
         setUp(myscenario.injectOpen(
-                atOnceUsers(20),
-                rampUsers(300).during(60) // Ramp up to 10,000 users over 120 seconds (2 minutes)
+//                atOnceUsers(350),
+                rampUsers(5000).during(60) // Ramp up to 10,000 users over 120 seconds (2 minutes)
         ).protocols(req));
     }
 }
