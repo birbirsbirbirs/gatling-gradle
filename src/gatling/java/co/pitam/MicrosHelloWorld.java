1f8b7c030eb11d21cf450ab50e0d77af020dc607
birbirs <birbirsbirbirs@gmail.com>
2025-07-11 14:58:15 -0500

    updated

--- src/gatling/java/co/pitam/MicrosHelloWorld.java
+++ src/gatling/java/co/pitam/MicrosHelloWorld.java
@@ -6,7 +6,6 @@ import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.rampUsers;
-import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 
@@ -20,11 +19,19 @@ public class MicrosHelloWorld extends Simulation {
                             status().is(200),
                             status().not(500)
                     )
-            );
+            ).pause(3)
+            .exec(http("/hero")
+                    .get("/hero")
+                    .check(
+                            status().is(200),
+                            status().not(500)
+                    )
+            )
+            ;
 
     {
         setUp(myscenario.injectOpen(
-                rampUsers(300*10*100).during(60*20)
+                rampUsers(1000*100).during(60*5)
         ).protocols(req));
     }
 }
